3e683722df062a33bd77048e927203f356c61426
김은행 <87305109+kimbank@users.noreply.github.com>
2021-11-24 17:35:29 +0900

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -58,6 +58,5 @@ def lambda_handler(event, context):
   return {
     'statusCode': 200,
     'headers': {'Access-Control-Allow-Origin': '*'},
-    # 'body': json.dumps(makingLottoNumberTemp),
     'body': json.dumps(data)
   }
